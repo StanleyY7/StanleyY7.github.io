08396dd3d4cc429e5d4572768b4bd9b59590fb46
StanleyY7 <stanleyyu7@gmail.com>
2023-04-13 23:07:15 +1000

    added animations based on if in view

--- js/script.js
+++ js/script.js
@@ -36,3 +36,64 @@ hamburger_menu.addEventListener("click", () => {
 });
 
 links.forEach((link) => link.addEventListener("click", () => closeMenu()));
+
+// If entity is in view trigger animations
+
+const element = document.getElementById("eShop");
+const element2 = document.getElementById("projects__img-container_img1");
+
+const fakeOS = document.getElementById("fakeOs");
+const fakeOsImage = document.getElementById("projects__img-container_img2");
+
+const gBooks = document.getElementById("gBooks");
+const gBooksImage = document.getElementById("projects__img-container_img3");
+
+const minesweeper = document.getElementById("minesweeper");
+const minesweeperImage = document.getElementById(
+  "projects__img-container_img4"
+);
+
+if (window.matchMedia("(min-width:1000px)").matches) {
+  const observer = new IntersectionObserver(
+    (entries) => {
+      if (entries[0].isIntersecting) {
+        element.classList.add("fade-in-left-text");
+
+        element2.classList.add("fade-in-left");
+        fakeOS.classList.add("fade-in-right-text");
+
+        fakeOsImage.classList.add("fade-in-right");
+        gBooks.classList.add("fade-in-left-text");
+
+        gBooksImage.classList.add("fade-in-left");
+        minesweeper.classList.add("fade-in-right-text");
+
+        minesweeperImage.classList.add("fade-in-right");
+      }
+    },
+    { threshold: [0.5] }
+  );
+
+  observer.observe(element);
+} else {
+  const observer = new IntersectionObserver(
+    (entries) => {
+      if (entries[0].isIntersecting) {
+        element2.classList.add("fade-in-left");
+        fakeOsImage.classList.add("fade-in-right");
+        gBooksImage.classList.add("fade-in-left");
+        minesweeperImage.classList.add("fade-in-right");
+      }
+    },
+    { threshold: [0.5] }
+  );
+
+  if ((element2.style.opacity = "1")) {
+    element.style.opacity = "1";
+    fakeOS.style.opacity = "1";
+    gBooks.style.opacity = "1";
+    minesweeper.style.opacity = "1";
+  }
+
+  observer.observe(element);
+}
